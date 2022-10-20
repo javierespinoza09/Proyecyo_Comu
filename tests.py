519f5d1647d5bf73f99d3fadaac990e67657a60d
Javier Espinoza <javierespriv44@gmail.com>
2022-10-19 23:04:28 -0600

    Prueba de Modulación

--- tests.py
+++ tests.py
@@ -16,18 +16,44 @@ t=np.linspace(0,1,len(wav))
 fc=85000
 #plt.plot(wav)
 #plt.show()
-ang=np.multiply(t_resamp,2*np.pi*fc)+resamp_wav*5e-5
-#ang=np.multiply(t,2*np.pi*fc)
-c=np.cos(ang)
-f, Pxx_den = scipy.signal.periodogram(c, fs_resamp)
-#plt.plot(c)
-plt.semilogy(f, Pxx_den)
-plt.ylim([1e-14, 1])
-plt.xlim([70e3, 100e3])
-plt.xlabel('frequency [Hz]')
-plt.ylabel('PSD [V**2/Hz]')
-plt.grid(True)
+
+ang1=np.multiply(t_resamp,2*np.pi*fc)
+c1=np.cos(ang1)
+ang2=np.multiply(t_resamp,2*np.pi*fc)+resamp_wav*2.5e-4
+c2=np.cos(ang2)
+
+f0, Pxx_den0 = scipy.signal.periodogram(resamp_wav, fs_resamp)
+f1, Pxx_den1 = scipy.signal.periodogram(c1, fs_resamp)
+f2, Pxx_den2 = scipy.signal.periodogram(c2, fs_resamp)
+
+
+fig, axs = plt.subplots(3)
+axs[0].semilogy(f0, Pxx_den0)
+axs[0].set_ylim([1, 1e9])
+axs[0].set_xlim([300, 700])
+axs[0].set_xlabel('frequency [Hz]')
+axs[0].set_ylabel('PSD [V**2/Hz]')
+axs[0].grid()
+
+axs[1].semilogy(f1, Pxx_den1)
+axs[1].set_ylim([1e-14, 1])
+axs[1].set_xlim([70e3, 100e3])
+axs[1].set_xlabel('frequency [Hz]')
+axs[1].set_ylabel('PSD [V**2/Hz]')
+axs[1].grid()
+
+axs[2].semilogy(f2, Pxx_den2)
+axs[2].set_ylim([1e-14, 1])
+axs[2].set_xlim([70e3, 100e3])
+axs[2].set_xlabel('frequency [Hz]')
+axs[2].set_ylabel('PSD [V**2/Hz]')
+axs[2].grid()
+
+
 plt.show()
+
+
+
 peaks_c=scipy.signal.find_peaks(c)
 print(peaks_c)
 #print(peaks)
